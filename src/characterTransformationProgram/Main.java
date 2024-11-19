ff320660b2e330c58d0ebcd2046b4d72274db047
Zetazone <37312796+Zetazone@users.noreply.github.com>
2024-11-19 16:48:33 +1100

    This is the first commit. This is just cloning from the remote repository, creating the file then committing the changes to remote.

---
+++ src/characterTransformationProgram/Main.java
@@ -0,0 +1,11 @@
+package characterTransformationProgram;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello WOrd");
+
+	}
+
+}
